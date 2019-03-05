0f496d9fac6b5e743fcfcc3e50523812be686a19
Deary <chickenflakes9@gmail.com>
2019-03-06 01:21:30 +0700

    'Finalization'

---
+++ public/precache-manifest.9f99b35f582727fc9de23ba5b0bdf7f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/spacex.ab10d000.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "70acc83e961ce1379318",
+    "url": "/static/js/main.70acc83e.chunk.js"
+  },
+  {
+    "revision": "35b720f79b0f8bb89d48",
+    "url": "/static/js/2.35b720f7.chunk.js"
+  },
+  {
+    "revision": "70acc83e961ce1379318",
+    "url": "/static/css/main.5faa586c.chunk.css"
+  },
+  {
+    "revision": "f2ee85542edff1507df53ea486bff54a",
+    "url": "/index.html"
+  }
+];
