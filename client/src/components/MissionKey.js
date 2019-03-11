3d75cc618c2d1c9787f6f5f3b48629d41a694daf
Deary <chickenflakes9@gmail.com>
2019-03-11 21:18:47 +0700

    Just change the sentences

--- client/src/components/MissionKey.js
+++ client/src/components/MissionKey.js
@@ -4,10 +4,10 @@ export default function MissionKey() {
   return (
     <div className="my-3">
       <p>
-        <span className="px-3 mr-2 bg-success" /> = Success
+        <span className="px-3 mr-2 bg-success" /> = Successful
       </p>
       <p>
-        <span className="px-3 mr-2 bg-danger" /> = Fail
+        <span className="px-3 mr-2 bg-danger" /> = Failed
       </p>
     </div>
   );
